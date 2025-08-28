6c69d910aaf92106c57b83aca45a444fb8df6f7d
Faizullah Al Fahim <faizullahalfahim@gmail.com>
2025-08-28 15:40:13 +0300

    added history and alert functionality

--- index.js
+++ index.js
@@ -1,14 +1,58 @@
+// heart icon functionality
 let icons = document.getElementsByClassName('heart-icon');
-let hearCountElement = document.getElementById('heart-count');
+let heartCountElement = document.getElementById('heart-count');
 let count= 0; 
 for (let icon of icons){
     icon.addEventListener("click",function(){
         count++
-        hearCountElement.innerText = count; 
+        heartCountElement.innerText = count; 
     })
-
 }
 
+// call button functionality
+
+let callHistory = []
+let coinCountElement = document.getElementById ('coin-count')
+let callBtns = document.getElementsByClassName('call-button')
+let coins = parseInt(coinCountElement.innerText ) 
+
+for (let btn of callBtns){
+    btn.addEventListener("click",function(){
+        if 
+        (coins < 20){
+                alert('No enough coins')
+                return; 
+        }
+        const card = btn.closest('.card')
+    const name = card.querySelector('.service-name').innerText;
+    const number= card.querySelector('.service-number').innerText;
+        coins = coins-20;
+        coinCountElement.innerText = coins; 
+         let history = {
+        name: name,
+        number: number,
+        time:new Date()
+    }
+      callHistory.push(history);
+      alert (`Service: ${history.name}/Number: ${history.number}`);
+
+    const historyContainer = document.getElementById('history-container')
+    // for(const history of callHistory){
+        const div = document.createElement('div')
+        div.innerHTML=`
+        <div class="flex bg-slate-200 rounded-[20px] justify-between p-2 m-2 ">
+            <div>
+                <p>${history.name}</p>
+                <p>${history.number}</p>
+            </div>
+            <div>
+                <p>${history.time}</p>
+            </div>
+        </div>`
+        historyContainer.appendChild(div);
+    
+    }) 
+}
 
 
 
